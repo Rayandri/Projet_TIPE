dcb73b95ecfdfdfa99504cc7a5de5416c64d8b05
Gwendal Saloum <gwendal.saloum@gmail.com>
2023-03-01 19:25:38 +0100

    Clean imports -> only from internet

--- ENX/main_ERC20.sol
+++ ENX/main_ERC20.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ENXToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("EnergyX", "ENX") {
@@ -12,5 +13,12 @@ contract ENXToken is ERC20 {
         return 2;
     }
 
+    function mint(address to, uint256 amount) external OnlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
 
 }
