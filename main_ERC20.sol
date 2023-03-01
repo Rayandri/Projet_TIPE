5758c0b03d032f759afd0444eabbe6e3ba37ab66
Gwendal Saloum <gwendal.saloum@gmail.com>
2023-03-01 17:02:46 +0100

    Base + decimal

--- main_ERC20.sol
+++ main_ERC20.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract ENXToken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("EnergyX", "ENX", 2) public {
+    constructor(uint256 initialSupply) ERC20("EnergyX", "ENX") public {
         _mint(msg.sender, initialSupply);
     }
 
+    function decimals() public view virtual override returns (uint8) { // override default decimals
+        return 2;
+    }
 
 }
